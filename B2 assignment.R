c61c0043962be5f9174fb76894266bd517b86792
Luic Damian <luicdamian@gmail.com>
2023-03-10 21:10:56 +0100

    Assignment

---
+++ B2 assignment.R
@@ -0,0 +1,9 @@
+# Explore the data and produce exploratory graphs
+
+# Build a linear model Spp vs Env without interactions and perform stepwise model selection and interpret the outputs
+
+# Build a linear model Spp vs Env with interactions and perform stepwise model selection and interpret the outputs
+
+# Perform an nMDS analysis on the total community and produce graph (hint: use the vegan package)
+
+# Perform an RDA analysis on the total community and produce graph (hint: use the vegan package)
