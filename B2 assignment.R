14f2a1596fce562c6d69d6d84c1c64adb220645d
Luic Damian <luicdamian@gmail.com>
2023-03-13 18:09:15 +0100

    Data exploration + forward selection model

--- B2 assignment.R
+++ B2 assignment.R
@@ -13,4 +13,32 @@
 pacman::p_load(here, tidyverse, ggplot2, car, GGally, emmeans)
 
 ## Read data
+
+### Raw data
 data = read.csv(here("data/ETX3_B2_multivariate_data2023.csv"))
+
+### Only species
+sp = data[1:20]
+
+### Only enviromental variables
+env = data[21:25]
+
+
+## Data exploration
+str(data)
+summary(data)
+head(data)
+
+# Collinearity checks
+(data_overview <- data%>%ggpairs(columns = 1:ncol(data), 
+                                   # mapping = ggplot2::aes(colour = island),
+                                    lower = list(continuous = wrap("smooth", alpha = 0.3, size = 0.7)))+
+    theme_classic())
+
+data_overview
+
+
+## Linear models
+
+## No interactions
+lm = lm()
